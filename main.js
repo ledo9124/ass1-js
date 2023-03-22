017f2fc9eadc4c7502e0355cdc1fbaa8106dfb93
ledo9124 <leminhdo904@gmail.com>
2023-03-22 11:26:02 +0700

    save1

---
+++ main.js
@@ -0,0 +1,20 @@
+var btnElement = document.querySelectorAll('.btn-form');
+var overlayElement = document.querySelector('.overlay');
+var hidenElement = document.querySelector('.hiden-form');
+var lengthbtn = btnElement.length;
+for (let i =0 ; i < lengthbtn ; ++i) {
+    btnElement[i].addEventListener('click' , function (e) {
+        overlayElement.style.display = 'block'; 
+        hidenElement.style.display = 'block'; 
+    });
+}
+
+document.querySelectorAll('li')[2].addEventListener('click' , function(e){
+    overlayElement.style.display = 'block'; 
+    hidenElement.style.display = 'block'; 
+})
+
+overlayElement.addEventListener('click' , function(e){
+    overlayElement.style.display = 'none'; 
+    hidenElement.style.display = 'none'; 
+})
